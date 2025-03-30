d4da81236e9cc84bf6b8fb7da53f7f45900be911
Bhawana Rokey <Bhawanarokey@gmail.com>
2025-03-30 11:07:37 +0530

    Numbers which is occuring odd number of times

---
+++ Element Occurring Odd Number of Times.py
@@ -0,0 +1,10 @@
+l=[1,3,2,3,2,1,2,3,2,1,2,3,2,1,3,3,1,3]
+s=set(l)
+print(s)
+for i in s:
+    count=0
+    for j in l:
+        if j==i:
+            count+=1
+    if count%2!=0:
+        print(f"{i} occurs odd number of times is {count}")
